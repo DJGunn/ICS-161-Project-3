3251f5d492effdd7797184811d491d76329cfa03
Unknown <DJGunn@uci.edu>
2018-03-15 07:09:27 -0700

    Super awesome mega main menu title awesomeness
    Yeah! Self-explanatory!

---
+++ Assets/Scripts/CloudScroller.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudScroller : MonoBehaviour {
+
+    public float scrollSpeed;
+
+    private Vector2 originalScale;
+
+    void Start()
+    {
+        originalScale = transform.localScale;
+
+        transform.position = new Vector2(Random.Range(-9f,9f), Random.Range(0f, 4f));
+        scrollSpeed = Random.Range(.5f, 2f);
+        transform.localScale = originalScale + new Vector2( Random.Range(-.05f, .05f), Random.Range(-.05f, .05f));
+    }
+
+    void Update()
+    {
+        transform.Translate(Time.deltaTime * scrollSpeed * Vector2.left);
+
+        if (transform.position.x < -13f) resetCloud();
+    }
+
+
+    private void resetCloud()
+    {
+        transform.position = new Vector2(13, Random.Range(0f, 4f));
+        scrollSpeed = Random.Range(.5f, 2f);
+        transform.localScale = originalScale + new Vector2(Random.Range(-.05f, .05f), Random.Range(-.05f, .05f));
+    }
+}
---
+++ Assets/Scripts/TitleScale.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScale : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float scale = .015f * (20f + Mathf.Cos(Time.time * (132f / 60) * Mathf.PI * 2));
+        transform.localScale = new Vector3(scale, scale, scale);
+    }
+}
