965d4dec942fccbde8b80964efd2eb152634d175
Tomas J. Zazueta <tzazueta@uci.edu>
2018-03-15 18:28:46 -0700

    AI test

---
+++ Assets/Scripts/Bulletpath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bulletpath : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*transform.Translate (Vector2. * Time.deltaTime * speed);*/
+		if (transform.position.x < -10||transform.position.x>10||transform.position.y<-5||transform.position.y>5) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/kanon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kanon : MonoBehaviour {
+	public GameObject player;
+	public int speed= 3;
+
+	public float coolDown;
+	public float waiter = 0.3f;
+	public Rigidbody2D bullet;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 localPosition = player.transform.position - transform.position;
+		localPosition = localPosition.normalized; 
+		//rotate to point at player
+		float angle = Mathf.Atan2(localPosition.y, localPosition.x) * Mathf.Rad2Deg-90;
+		Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+		transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
+		if (speed == 1) {
+			if (Time.time >= coolDown) {
+				Rigidbody2D bulletInstance;
+				bulletInstance = Instantiate (bullet, transform.position, Quaternion.identity) as Rigidbody2D;
+				bulletInstance.AddForce (transform.up * 500f);
+				coolDown = Time.time +waiter;
+			}
+		}
+	}
+}
